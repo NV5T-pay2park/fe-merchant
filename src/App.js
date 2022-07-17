05ddb2b3660596eeda1ed38650b87d8f2533d825
Tien Thanh <thanhndt@vng.com.vn>
2022-07-17 17:20:39 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// @mui material components
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-function App() {
+// Material Kit 2 React themes
+// import theme from "assets/theme";
+import AboutUs from "./pages/AboutUs";
+
+const theme = createTheme();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AboutUs />
+    </ThemeProvider>
   );
 }
-
-export default App;
